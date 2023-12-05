cf21098273db0cd588488f49b595c68442af3d4a
Diviner <sqq_job@sina.com>
2023-12-05 15:37:34 +0800

    feat: 配置IncLine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -36,7 +36,7 @@ return {
         $$$$$$$  |$$ |   \$  /   $$ |$$ |  $$ |\$$$$$$$\ $$ |           \$  /   $$ |$$ | $$ | $$ |
         \_______/ \__|    \_/    \__|\__|  \__| \_______|\__|            \_/    \__|\__| \__| \__|
       ]]
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      logo = string.rep("\n", 2) .. logo .. "\n\n"
       local opts = {
         theme = "doom",
         hide = {
@@ -74,6 +74,18 @@ return {
               desc = " Config",
               icon = " ",
               key = "c",
+            },
+            {
+              action = 'lua require("persistence").load()',
+              desc = " Restore Session",
+              icon = " ",
+              key = "s",
+            },
+            {
+              action = "LazyExtras",
+              desc = " Lazy Extras",
+              icon = " ",
+              key = "x",
             },
             {
               action = "Lazy",
@@ -125,6 +137,54 @@ return {
           skip = true,
         },
       })
+    end,
+  },
+  -- 配置 IncLine
+  {
+    "b0o/incline.nvim",
+    opts = function()
+      local function get_diagnostic_label(props)
+        local icons = {
+          Error = "",
+          Warn = "",
+          Info = "",
+          Hint = "",
+        }
+        local label = {}
+        for severity, icon in pairs(icons) do
+          local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+          if n > 0 then
+            table.insert(label, { icon .. " " .. n .. " ", group = "DiagnosticSign" .. severity })
+          end
+        end
+        return label
+      end
+
+      local opts = {
+        debounce_threshold = { falling = 500, rising = 250 },
+        render = function(props)
+          local bufname = vim.api.nvim_buf_get_name(props.buf)
+          local filename = vim.fn.fnamemodify(bufname, ":t")
+          local diagnostics = get_diagnostic_label(props)
+          local modified = vim.api.nvim_buf_get_option(props.buf, "modified") and "bold,italic" or "None"
+          local filetype_icon, color = require("nvim-web-devicons").get_icon_color(filename)
+
+          local buffer = {
+            { filetype_icon, guifg = color },
+            { " " },
+            { filename, gui = modified },
+          }
+
+          if #diagnostics > 0 then
+            table.insert(diagnostics, { "| ", guifg = "grey" })
+          end
+          for _, buffer_ in ipairs(buffer) do
+            table.insert(diagnostics, buffer_)
+          end
+          return diagnostics
+        end,
+      }
+      return opts
     end,
   },
 }
