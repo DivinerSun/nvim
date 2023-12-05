1075e646df21580e9c921be169f3ae7f5d3fe3fd
Diviner <sqq_job@sina.com>
2023-12-05 17:23:52 +0800

    feat: 内部终端

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -21,4 +21,31 @@ return {
     "mg979/vim-visual-multi",
     event = "BufWinEnter",
   },
+  -- 内部终端
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      size = 20,
+      open_mapping = [[<C-t>]],
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = "float",
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = "curved",
+        winblend = 0,
+        highlights = {
+          border = "Normal",
+          background = "Normal",
+        },
+      },
+    },
+  },
 }
