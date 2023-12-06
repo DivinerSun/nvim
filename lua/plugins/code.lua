d0ecf6e9e98341c4e800fb1b04187c01d2017068
Diviner <sqq_job@sina.com>
2023-12-06 11:07:01 +0800

    feat: 配置颜色值能显示出当前颜色

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -48,4 +48,26 @@ return {
       },
     },
   },
+  -- 颜色插件
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = "BufRead",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+        RGB = true,
+        RRGGBB = true,
+        names = true,
+        RRGGBBAA = true,
+        AARRGGBB = true,
+        rgb_fn = true,
+        hsl_fn = true,
+        css = true,
+        css_fn = true,
+        mode = "background",
+        sass = { enable = true, parsers = { "css" } },
+        virtualtext = "■",
+      },
+    },
+  },
 }
