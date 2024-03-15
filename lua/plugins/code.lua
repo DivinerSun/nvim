f4c43cd490384429780d8811804c449a208b0648
Diviner <sqq_job@sina.com>
2024-03-15 16:36:31 +0800

    feat: 添加flutter-tools配置

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -76,6 +76,55 @@ return {
     event = "BufWinEnter",
     opts = {},
   },
+  -- Flutter
+  {
+    "akinsho/flutter-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "stevearc/dressing.nvim" },
+    config = function()
+      require("flutter-tools").setup({
+        flutter_path = "/Users/diviner/fvm/default/bin/flutter",
+        debugger = {
+          -- make these two params true to enable debug mode
+          enabled = false,
+          run_via_dap = false,
+          register_configurations = function(_)
+            require("dap").adapters.dart = {
+              type = "executable",
+              command = vim.fn.stdpath("data") .. "/mason/bin/dart-debug-adapter",
+              args = { "flutter" },
+            }
+            require("dap").configurations.dart = {
+              {
+                type = "dart",
+                request = "launch",
+                name = "Launch flutter",
+                dartSdkPath = "/Users/diviner/fvm/default/bin/cache/dart-sdk/",
+                flutterSdkPath = "/Users/diviner/fvm/default",
+                program = "${workspaceFolder}/lib/main.dart",
+                cwd = "${workspaceFolder}",
+              },
+            }
+            -- uncomment below line if you've launch.json file already in your vscode setup
+            -- require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+        closing_tags = {
+          highlight = "ErrorMsg",
+          prefix = " ",
+          enabled = true,
+        },
+        dev_log = {
+          -- toggle it when you run without DAP
+          enabled = false,
+          open_cmd = "tabedit",
+        },
+        lsp = {
+          -- on_attach = require("lazyvim.util.lsp").on_attach,
+          capabilities = require("cmp_nvim_lsp").default_capabilities,
+        },
+      })
+    end,
+  },
   -- Dart插件
   {
     "dart-lang/dart-vim-plugin",
