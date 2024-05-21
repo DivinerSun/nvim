dbb04cb71d1e96d1fd7c0b27e238b2103f528b59
Diviner <sqq_job@sina.com>
2024-05-21 13:47:30 +0800

    feat: update

--- README.md
+++ README.md
@@ -4,3 +4,19 @@
 
 - Github 地址：[LazyVim](https://github.com/LazyVim/LazyVim).
 - 官网文档地址：[LazyVim](https://www.lazyvim.org/).
+
+- 添加Dap调试插件
+
+1. 创建日志文件
+
+根据个人情况
+/Users/diviner/.local/share/nvim/dap_vscode_js.log
+
+```bash
+cd /Users/diviner/.local/share/nvim/lazy
+git clone https://github.com/microsoft/vscode-js-debug
+cd vscode-js-debug
+npm install --legacy-peer-deps
+npx gulp vsDebugServerBundle
+mv dist out
+```
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -295,15 +295,6 @@ return {
     opts = {
       servers = { eslint = {}, dartls = {} },
       setup = {
-        eslint = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
         clangd = function(_, opts)
           opts.capabilities.offsetEncoding = { "utf-16" }
         end,
